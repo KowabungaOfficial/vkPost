242b86f67f9cb5020b0c781f56d22f2369762a05
Kowabunga <77234307+KowabungaOfficial@users.noreply.github.com>
2025-03-23 16:19:34 -0500

    x11 and Wayland Support

--- src/keyboard_input.cpp
+++ src/keyboard_input.cpp
@@ -1,20 +1,38 @@
 #include "keyboard_input.hpp"
-
 #include "logger.hpp"
 
-// TODO build without X11
 #ifndef VKPOST_X11
 #define VKPOST_X11 1
+#endif
+
+#ifndef VKPOST_WAYLAND
+#define VKPOST_WAYLAND 1
 #endif
 
 #if VKPOST_X11
 #include "keyboard_input_x11.hpp"
 #endif
 
+#if VKPOST_WAYLAND
+#include "keyboard_input_wayland.hpp"
+#endif
+
 namespace vkPost
 {
+    // Helper function to check if Wayland is active
+    bool isWaylandActive()
+    {
+        const char* sessionType = getenv("XDG_SESSION_TYPE");
+        return sessionType && strcmp(sessionType, "wayland") == 0;
+    }
+
     uint32_t convertToKeySym(std::string key)
     {
+#if VKPOST_WAYLAND
+        if (isWaylandActive()) {
+            return convertToKeySymWayland(key);
+        }
+#endif
 #if VKPOST_X11
         return convertToKeySymX11(key);
 #endif
@@ -23,6 +41,11 @@ namespace vkPost
 
     bool isKeyPressed(uint32_t ks)
     {
+#if VKPOST_WAYLAND
+        if (isWaylandActive()) {
+            return isKeyPressedWayland(ks);
+        }
+#endif
 #if VKPOST_X11
         return isKeyPressedX11(ks);
 #endif
