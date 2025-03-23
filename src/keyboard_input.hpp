a496bad5c9e1b5cff0b48f4f295a4c19547ab392
Kowabunga <77234307+KowabungaOfficial@users.noreply.github.com>
2025-03-23 16:20:04 -0500

    Update keyboard_input.hpp

--- src/keyboard_input.hpp
+++ src/keyboard_input.hpp
@@ -7,4 +7,9 @@ namespace vkPost
 {
     uint32_t convertToKeySym(std::string key);
     bool     isKeyPressed(uint32_t ks);
+#if VKPOST_WAYLAND
+    uint32_t convertToKeySymWayland(std::string key);
+    bool     isKeyPressedWayland(uint32_t ks);
+    void     init_wayland_data();
+#endif
 } // namespace vkPost
