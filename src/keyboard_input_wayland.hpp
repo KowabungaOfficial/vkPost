c1b430f2c6d6cfff532d526d0c789313997c7016
Kowabunga <77234307+KowabungaOfficial@users.noreply.github.com>
2025-03-23 20:20:51 -0500

    Update keyboard_input_wayland.hpp

--- src/keyboard_input_wayland.hpp
+++ src/keyboard_input_wayland.hpp
@@ -1,10 +1,9 @@
-#ifndef KEYBOARD_INPUT_WAYLAND_HPP
-#define KEYBOARD_INPUT_WAYLAND_HPP
+#pragma once
 
 #include <cstdint>
 #include <string>
 
-uint32_t convertToKeySymWayland(std::string key);
-bool isKeyPressedWayland(uint32_t ks);
-
-#endif // KEYBOARD_INPUT_WAYLAND_HPP
+namespace vkPost {
+    uint32_t convertToKeySymWayland(std::string key);
+    bool isKeyPressedWayland(uint32_t ks);
+}
