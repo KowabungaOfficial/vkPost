dd446db69ffdd49311f1e63f11d6c7cd3a909525
Kowabunga <77234307+KowabungaOfficial@users.noreply.github.com>
2025-03-23 20:04:28 -0500

    Update keyboard_input.hpp

--- src/keyboard_input.hpp
+++ src/keyboard_input.hpp
@@ -2,24 +2,9 @@
 
 #include <cstdint>
 #include <string>
-#include "config.hpp"
 
 namespace vkPost
 {
-    enum class InputMethod {
-        X11,
-        Wayland,
-        NoInput
-    };
-
     uint32_t convertToKeySym(std::string key);
     bool     isKeyPressed(uint32_t ks);
-#if VKPOST_WAYLAND
-    uint32_t convertToKeySymWayland(std::string key);
-    bool     isKeyPressedWayland(uint32_t ks);
-    void     init_wayland_data();
-#endif
-
-    void setInputMethodBasedOnConfig(const Config& config);
-    void initializeKeyboardInput();
 } // namespace vkPost
