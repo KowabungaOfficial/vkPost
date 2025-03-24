af91aab8cc21c8e7e7341c7a020d5910d617cd78
Kowabunga <77234307+KowabungaOfficial@users.noreply.github.com>
2025-03-23 20:17:33 -0500

    Update keyboard_input_wayland.hpp

--- src/keyboard_input_wayland.hpp
+++ src/keyboard_input_wayland.hpp
@@ -1,1 +1,10 @@
-empty for now
+#ifndef KEYBOARD_INPUT_WAYLAND_HPP
+#define KEYBOARD_INPUT_WAYLAND_HPP
+
+#include <cstdint>
+#include <string>
+
+uint32_t convertToKeySymWayland(std::string key);
+bool isKeyPressedWayland(uint32_t ks);
+
+#endif // KEYBOARD_INPUT_WAYLAND_HPP
