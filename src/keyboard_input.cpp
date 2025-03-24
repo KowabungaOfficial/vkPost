2eca7cb1a426c6e9c9d8da17c805acacf4c562b4
Kowabunga <77234307+KowabungaOfficial@users.noreply.github.com>
2025-03-23 20:26:39 -0500

    Update keyboard_input.cpp

--- src/keyboard_input.cpp
+++ src/keyboard_input.cpp
@@ -1,31 +1,48 @@
 #include "keyboard_input.hpp"
-
+#include "display_server.hpp"
 #include "logger.hpp"
 
-// TODO build without X11
 #ifndef VKPOST_X11
 #define VKPOST_X11 1
+#endif
+#ifndef VKPOST_WAYLAND
+#define VKPOST_WAYLAND 1
 #endif
 
 #if VKPOST_X11
 #include "keyboard_input_x11.hpp"
 #endif
-
-namespace vkPost
-{
-    uint32_t convertToKeySym(std::string key)
-    {
-#if VKPOST_X11
-        return convertToKeySymX11(key);
+#if VKPOST_WAYLAND
+#include "keyboard_input_wayland.hpp"
 #endif
+
+namespace vkPost {
+    uint32_t convertToKeySym(DisplayServer ds, std::string key) {
+#if VKPOST_X11
+        if (ds == DisplayServer::X11) {
+            return convertToKeySymX11(key);
+        }
+#endif
+#if VKPOST_WAYLAND
+        if (ds == DisplayServer::WAYLAND) {
+            return convertToKeySymWayland(key);
+        }
+#endif
+        Logger::err("No display server support for key conversion");
         return 0u;
     }
 
-    bool isKeyPressed(uint32_t ks)
-    {
+    bool isKeyPressed(DisplayServer ds, uint32_t ks) {
 #if VKPOST_X11
-        return isKeyPressedX11(ks);
+        if (ds == DisplayServer::X11) {
+            return isKeyPressedX11(ks);
+        }
+#endif
+#if VKPOST_WAYLAND
+        if (ds == DisplayServer::WAYLAND) {
+            return isKeyPressedWayland(ks);
+        }
 #endif
         return false;
     }
-} // namespace vkPost
+}
